80ab94536ce7aee0724364a7b0b63f11e195b1cd
Dronian <andreyivfgh567@gmail.com>
2025-09-27 10:33:29 +0400

    Update task_1.java

--- src/main/java/org/kniiit/lab_1/task_1.java
+++ src/main/java/org/kniiit/lab_1/task_1.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            System.out.print(i + " ");
+            System.out.print(i + ", ");
             if (i % 10 == 0) {
                 System.out.println();
             }
         }
+        System.out.print(100);
     }
 }
