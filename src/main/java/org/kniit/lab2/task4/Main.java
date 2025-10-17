e5c96a427308685a730ead6aacbf6d451c5a99d1
Dronian <andreyivfgh567@gmail.com>
2025-10-17 18:25:26 +0400

    Update Main.java

--- src/main/java/org/kniit/lab2/task4/Main.java
+++ src/main/java/org/kniit/lab2/task4/Main.java
@@ -1,15 +1,30 @@
-public class Test {
+public class Main {
     public static void main(String[] args) {
         Container container = new Container(1000);
-        
+
         Shape sphere = new Sphere(5);
-        Shape cube = new Cube(3);  
-        Shape cylinder = new Cylinder(2, 4);
-        
+        Shape cube = new Cube(4);
+        Shape cylinder = new Cylinder(3, 5);
+
         System.out.println(container.add(sphere));
         System.out.println(container.add(cube));
         System.out.println(container.add(cylinder));
-        
-        System.out.println("Оставшийся объем: " + container.getRemainingVolume());
+
+        System.out.println(container.getRemainingVolume() + "\n");
+
+        System.out.println(container.add(sphere));
+        System.out.println(container.add(cube));
+        System.out.println(container.add(cylinder));
+
+        System.out.println(container.getRemainingVolume() + "\n");
+
+        System.out.println(container.add(cube));
+        System.out.println(container.add(cylinder));
+
+        System.out.println(container.getRemainingVolume() + "\n");
+
+        System.out.println(container.add(cube));
+
+        System.out.println(container.getRemainingVolume());
     }
 }
